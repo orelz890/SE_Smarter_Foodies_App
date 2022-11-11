b7afbe2149beb56d07ddace86404a0d43fb348bd
Orel Zamler <orelzx13@gmail.com>
2022-11-11 13:55:00 +0200

    First commit

---
+++ app/src/main/java/com/example/smarter_foodies/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.smarter_foodies;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    FirebaseFirestore firestore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firestore = FirebaseFirestore.getInstance();
+        Map<String, Object> users = new HashMap<>();
+        users.put("aaa", "orel");
+        users.put("bbb", "zamler");
+        users.put("firstName", "Eilon");
+        users.put("LastName", "Barashi");
+        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+}
