1707d8a47ccde6c5f884823fc54e1d5098f92f9e
Orel Zamler <orelzx13@gmail.com>
2022-11-11 14:27:56 +0200

    trying to sync

--- app/src/main/java/com/example/smarter_foodies/MainActivity.java
+++ app/src/main/java/com/example/smarter_foodies/MainActivity.java
@@ -40,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
             }
         });
-
     }
 
 }
