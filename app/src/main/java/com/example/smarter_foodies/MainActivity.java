461f0cfbf3e7c32ac611de482dd027572d111f68
Orel Zamler <orelzx13@gmail.com>
2022-11-15 00:36:44 +0200

    added login and register abilities(not finished- apply as chef not ready)

--- app/src/main/java/com/example/smarter_foodies/MainActivity.java
+++ app/src/main/java/com/example/smarter_foodies/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.smarter_foodies;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -16,32 +21,30 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    FirebaseFirestore firestore;
+    Button btnLogOut;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        firestore = FirebaseFirestore.getInstance();
-        Map<String, Object> users = new HashMap<>();
-        users.put("aaa", "orel");
-        users.put("bbb", "zamler");
-        users.put("firstName", "Eilon");
-        users.put("LastName", "Barashi");
-        users.put("password1", "1234");
-        users.put("password2", "5678");
-        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-            @Override
-            public void onSuccess(DocumentReference documentReference) {
-                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
-            }
+
+        btnLogOut = findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogOut.setOnClickListener(view ->{
+            mAuth.signOut();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
         });
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
+    }
 }
