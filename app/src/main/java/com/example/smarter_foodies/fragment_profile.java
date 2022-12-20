e247bf9e4c8d7ca4a251cbd6caf8168452593897
aviel780 <aviel780@gmail.com>
2022-12-20 16:30:18 +0200

    update profile

--- app/src/main/java/com/example/smarter_foodies/fragment_profile.java
+++ app/src/main/java/com/example/smarter_foodies/fragment_profile.java
@@ -16,50 +16,64 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class fragment_profile extends Fragment {
+    private FirebaseAuth mAuth;
+
     ImageView image_profile,options;
     TextView posts,followers,following,fullname,bio,username;
-    Button edit_profile;
-
+    Button update_profile;
     FirebaseUser firebaseUser;
     String profileid;
-
     ImageButton my_fotos,saved_fotos;
 
     public View onCreatView(LayoutInflater inflater, ViewGroup continer, Bundle savedinstancestate){
     View view = inflater.inflate(R.layout.activity_fragment_profile,continer,false);
     firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        SharedPreferences prefs = getContext().getSharedPreferences("PREFS", Context.MODE_PRIVATE);
-        profileid =prefs.getString("profileid","none");
-        image_profile = view.findViewById(R.id.image_profile);
-        options = view.findViewById(R.id.options);
-        posts = view.findViewById(R.id.posts);
-        followers = view.findViewById(R.id.followers);
-        following = view.findViewById(R.id.following);
-        fullname = view.findViewById(R.id.fullname);
-        bio = view.findViewById(R.id.bio);
-        username = view.findViewById(R.id.username);
-        edit_profile = view.findViewById(R.id.edit_profile);
-        my_fotos = view.findViewById(R.id.my_fotos);
-        saved_fotos = view.findViewById(R.id.save_fotos);
+    SharedPreferences prefs = getContext().getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+    profileid =prefs.getString("profileid","none");
+    image_profile = view.findViewById(R.id.image_profile);
 
-        edit_profile.setOnClickListener(new View.OnClickListener() {
+    posts = view.findViewById(R.id.posts);
+    followers = view.findViewById(R.id.followers);
+    following = view.findViewById(R.id.following);
+    fullname = view.findViewById(R.id.full_name_pp);
+
+    username = view.findViewById(R.id.username);
+    update_profile = view.findViewById(R.id.update_profile);
+    my_fotos = view.findViewById(R.id.my_fotos);
+    saved_fotos = view.findViewById(R.id.save_fotos);
+
+
+
+    update_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String btn = edit_profile.getText().toString();
+                FirebaseUser fuser = mAuth.getCurrentUser();
+                if (fuser != null) {
+                    String uid = fuser.getUid();
+                    DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("users").child(uid);
+                    String name  =  reference.child("Nickname").toString();
+                    String email = reference.child("Email").toString();
+                    String isChef = reference.child("isChef").toString();
+                    String type_food = reference.child("Type_food").toString();
+                    String favorite = reference.child("favorite").toString();
 
-                if (btn.equals("Edit Profile")){
 
-                }else if (btn.equals("follow")){
-                    FirebaseDatabase.getInstance().getReference().child("Follow").child(firebaseUser.getUid()).child("following").
-                            child(firebaseUser.getUid()).setValue("true");}
 
+                }
 
             }
-        });
+        });{
+
+
+
+
+
+        }
 
         return view;
     }
